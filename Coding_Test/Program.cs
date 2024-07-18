3a957d5379106e4dec2c248948bdba9d5ef957a8
jiyeonUning <yeon14735@gmail.com>
2024-07-18 17:52:10 +0900

    문제.두 수의 차
    정수 num1과 num2가 주어질 때, num1에서 num2를 뺀 값을 return하도록 soltuion 함수를 완성해주세요.
    
    문제 출처:
    프로그래머스 코딩 테스트-두 수의 차

---
+++ Coding_Test/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Coding_Test
+{
+    internal class Program
+    {
+        // 정수 num1과 num2가 주어질 때, num1에서 num2를 뺀 값을 return하도록 soltuion 함수를 완성해주세요.
+
+        // 제한 사항
+        // -50000 ≤ num1 ≤ 50000
+        // -50000 ≤ num2 ≤ 50000
+
+        public class Solution
+        {
+            public int solution(int num1, int num2)   // 정수 선언
+            {
+                int answer = 0;
+
+                if (-50000 <= num1 && num1 <= 50000   // 제한 걸어주기
+                 && -50000 <= num2 && num2 <= 50000)
+                { answer = num1 - num2; }
+
+                return answer;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
